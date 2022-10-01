3b4990fc091711845b4b03168c4132e0feca949f
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2022-10-01 01:08:32 -1000

    Website planning doc

---
+++ lesson3/scripts/footer.js
@@ -0,0 +1,6 @@
+//Current year
+const y = new Date().getFullYear(); 
+document.getElementById("currentyear").innerHTML = y
+
+//Last updated 
+document.getElementById('lastupdate').textContent = document.lastModified
