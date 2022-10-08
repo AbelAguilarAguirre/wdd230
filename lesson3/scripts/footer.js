e6a8c4778e41f354182a339cd79e3be323a14df6
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2022-10-08 03:39:59 -1000

    chamber home page layout, header, footer, and nav

--- lesson3/scripts/footer.js
+++ lesson3/scripts/footer.js
@@ -1,6 +1,6 @@
 //Current year
 const y = new Date().getFullYear(); 
-document.getElementById("currentyear").innerHTML = y
+document.getElementById("currentyear").innerHTML = y;
 
 //Last updated 
-document.getElementById('lastupdate').textContent = document.lastModified
+document.getElementById('lastupdate').textContent = document.lastModified;
