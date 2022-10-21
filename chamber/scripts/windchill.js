ba9df4bb7967e1945d62598128f44a97fb39bdd7
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2022-10-20 19:03:56 -1000

    weather

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,13 @@
+//wind chill calculator
+const temperature = parseFloat(document.querySelector("#temperature").textContent);
+const windSpeed = parseFloat(document.querySelector("#windSpeed").textContent);
+let windChill = document.querySelector("#windChill");
+
+let result = 0;
+if (temperature <=50 && windSpeed > 3) {
+    result = 35.74 + 0.6215 * temperature - 35.75 * (windSpeed**0.16) + 0.4275 * temperature * (windSpeed**0.16)
+    windChill.textContent = result.toFixed(2) + "°F";
+};
+
+
+console.log(temperature, windSpeed, result)
