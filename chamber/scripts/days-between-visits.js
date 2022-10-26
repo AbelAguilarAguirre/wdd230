d04c6ade337a837db7c7a1c9d2b52401663eaba1
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2022-10-25 20:28:05 -1000

    JS time stamp

--- chamber/scripts/days-between-visits.js
+++ chamber/scripts/days-between-visits.js
@@ -1,9 +1,11 @@
 const visitDisplay = document.querySelector("#lastvisit")
-let timeStamp = Date(window.localStorage.getItem("visit1"));
+let timeStamp = Date.now(window.localStorage.getItem("visit1"));
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
 if (numVisits !== 0) {
-	timeStamp = timeStamp-timeStamp
-    visitDisplay.textContent = timeStamp
+	timeStamp = parseInt(Date.now()) - parseInt(timeStamp)
+    localStorage.setItem("visit1", timeStamp)
+    const days = Math.floor(timeStamp / (24*60*60*1000));
+    visitDisplay.textContent = days;
 } else {
 	visitDisplay.textContent = "This is your first visit!";
 }
