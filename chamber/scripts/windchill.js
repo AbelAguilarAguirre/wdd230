6ce3a4c0365a4d4bfb6ccbc586fedc47e511218d
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2022-10-26 18:09:51 -1000

    responsive design fixes

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -7,7 +7,9 @@ let result = 0;
 if (temperature <=50 && windSpeed > 3) {
     result = 35.74 + 0.6215 * temperature - 35.75 * (windSpeed**0.16) + 0.4275 * temperature * (windSpeed**0.16)
     windChill.textContent = result.toFixed(2) + "°F";
-};
+} else {
+    windChill.textContent = "N/A"
+}
 
 
 console.log(temperature, windSpeed, result)
