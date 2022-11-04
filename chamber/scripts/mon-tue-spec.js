30cfe036d2ff82a79d2e9560c3bc0ef161f91bb8
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2022-11-03 23:33:11 -1000

    join page

--- chamber/scripts/mon-tue-spec.js
+++ chamber/scripts/mon-tue-spec.js
@@ -1,7 +1,6 @@
 //display a banner on Mondays or Tuesdays only at the very top of the page that says "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
 const date = new Date();
 const currentDay = date.getDay();
-
 document.getElementById("specialbanner").style.display = "none";
 if (currentDay >= 1 && currentDay <= 2) {
     document.getElementById("specialbanner").style.display = "block";
@@ -14,3 +13,5 @@ document.getElementById("currentyear").innerHTML = currentYear;
 //Todays date
 document.getElementById('date').innerHTML = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
 
+//current date and time
+document.getElementById('datetime').innerHTML = date
---
+++ chamber/scripts/regex.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('#form');
+const input = document.querySelector('#business');
+const output = document.querySelector('#output');
+
+const re = /^[ \w-]{7,}/;
+
+function testInfo(businessInput) {
+    const ok = re.exec(businessInput.value);
+
+    output.textContent = !ok
+        ? `${businessInput.value} isn't written with the accepted alpha characters, hyphens, and spaces with a minimum of seven characters`:
+        ``
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    testInfo(input);
+});
