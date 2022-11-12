56461104d9d8e015bec4543e1715f74f7cc477ad
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2022-11-12 00:29:36 -1000

    business directory

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,40 @@
+const requestJSON = "/chamber/json/data.json"
+const grid = document.querySelector('.grid');
+
+fetch(requestJSON)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+    const businesses = jsonObject['businesses'];
+    businesses.forEach(displayBusinesses);
+  });
+
+  function displayBusinesses(business) {
+    let card = document.createElement('section');
+    let h3 = document.createElement('h3');
+    let p1 = document.createElement('p')
+    let p2 = document.createElement('p')
+    let p3 = document.createElement('p')
+    let logo = document.createElement('img');
+  
+    h3.innerHTML = `<h3>${business.name}</h3>`
+    p1.textContent = `${business.address}`
+    p2.textContent = `${business.phone}`
+    p3.textContent = `${business.url}`
+    logo.setAttribute('src', business.image);
+    logo.setAttribute('alt', `${business.name}'s Logo`);
+    logo.setAttribute('loading', 'lazy');
+  
+    card.appendChild(logo);
+    card.appendChild(h3);
+    card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(p3);
+    
+  
+    document.querySelector('.grid').appendChild(card);
+}
+
+
